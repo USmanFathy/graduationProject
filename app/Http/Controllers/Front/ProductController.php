88fd1b06b579664b0ff98cab42d457b55d7972c0
anas nashat <aanasht@gmail.com>
2024-03-17 04:59:37 +0200

    add all the products function needed and add the filters to it

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        $products = QueryBuilder::for(Product::class)
+            ->with('category')
+            ->allowedFilters(['category.slug','name','price'])
+            ->allowedSorts(['name', 'price'])
+
+            ->paginate(12);
+        return view('front.products.products',compact('products'));
 
     }
 
@@ -21,5 +31,23 @@ class ProductController extends Controller
         }
 
         return view('front.products.show' , compact('product'));
+    }
+
+    public function productsFilters(Category $category)
+    {
+        $products = QueryBuilder::for(Product::class)
+            ->with('category')
+            ->whereHas('category', function ($query) use ($category) { // Filter by category name (adjust LIKE operator if needed)
+                $query->where('name', 'like', '%' . $category . '%');
+            })
+            ->paginate(20);
+
+//        dd($products);
+
+//        $products = Product::with(['category'])
+//            ->where('category_id','=',$category->id)
+//            ->paginate(20)
+
+        return view('front.products.products' , compact('products'));
     }
 }
