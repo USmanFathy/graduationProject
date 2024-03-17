c6bfea600255c2eb713cb5ca3befd0223ad99d42
anas nashat <aanasht@gmail.com>
2024-03-18 01:19:49 +0200

    edite front

--- app/Helpers/Categoray.php
+++ app/Helpers/Categoray.php
@@ -6,8 +6,6 @@ use App\Models\Category;
 if (!function_exists('getCategoriesWithSubcategories')) {
     function getCategoriesWithSubcategories()
     {
-        return Category::with('subcategories')->with('products')
-
-            ->get();
+        return  Category::with('subcategories')->with('products')->get();
     }
 }
