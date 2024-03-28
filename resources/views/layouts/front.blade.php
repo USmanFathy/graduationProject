f0a2144373999a24a58d89d12d6b3b73f36ae0ca
usman ahmed fathy <91374170+USmanFathy@users.noreply.github.com>
2024-03-28 15:54:46 +0200

    updates

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,14 +7,14 @@
     <title>{{$title}}</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.svg')}}" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{secure_asset('assets/images/favicon.svg')}}" />
 
     <!-- ========================= CSS here ========================= -->
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/LineIcons.3.0.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/tiny-slider.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/glightbox.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
+    <link rel="stylesheet" href="{{secure_asset('assets/css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{secure_asset('assets/css/LineIcons.3.0.css')}}" />
+    <link rel="stylesheet" href="{{secure_asset('assets/css/tiny-slider.css')}}" />
+    <link rel="stylesheet" href="{{secure_asset('assets/css/glightbox.min.css')}}" />
+    <link rel="stylesheet" href="{{secure_asset('assets/css/main.css')}}" />
     @stack('css')
 
 </head>
